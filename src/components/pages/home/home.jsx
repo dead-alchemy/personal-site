d9a8c3c1bdd53658867daca03dd2d74bd46885ca
david <dlonicholas@gmail.com>
2021-03-31 22:33:22 -0600

    Home changes reverted.

--- src/components/pages/home/home.jsx
+++ src/components/pages/home/home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
 				<Row className="m-2">
 					<h1>Hello!</h1>
 				</Row>
-				<Row className="main_home_row m-2"></Row>
+				<Row className="main_home_row"></Row>
 				<Row className="m-2">
 					<p>
 						Welcome to my little site. Here is a collection of my

e7b7b11ac2005210d72dc11581ae877b20baabed
david <dlonicholas@gmail.com>
2021-04-02 22:06:15 -0600

    Outriders upload.

--- src/components/pages/entries/entries.jsx
+++ src/components/pages/entries/entries.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {Container} from "react-bootstrap";
 import Initial from "./posts/intial";
 import HelloPets from "./posts/hello_pets";
+import OutridersImpressions from "./posts/outriders_impressions";
 
 const Entries = (props) => {
 	let {id} = props.match.params;
@@ -10,6 +11,8 @@ const Entries = (props) => {
 			<Container className="pt-3">
 				{id === "2021-03-27" && <Initial />}
 				{id === "hello_pets" && <HelloPets />}
+				{id === "outriders_early" && <OutridersImpressions />}
+
 				<h4>Have a great day!</h4>
 			</Container>
 		</div>
--- src/service/service.js
+++ src/service/service.js
@@ -4,6 +4,7 @@ export function getEntries() {
 	let entries = [
 		{date: "2021-03-27", display: "Inital Blog Post"},
 		{date: "hello_pets", display: "Meet My Pets"},
+		{date: "outriders_early", display: "Outriders Early Impressions"},
 	];
 
 	return entries;

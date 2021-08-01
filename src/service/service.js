b38354269bcb7fe52f0d2eebf1964fe56ac04ded
david <dlonicholas@gmail.com>
2021-08-01 16:19:40 -0600

    new blog post.

--- src/service/service.js
+++ src/service/service.js
@@ -5,6 +5,7 @@ export function getEntries() {
 		{date: "2021-03-27", display: "Inital Blog Post"},
 		{date: "hello_pets", display: "Meet My Pets"},
 		{date: "outriders_early", display: "Outriders Early Impressions"},
+		{date: "flowers_for_m[A]chines", display: "NieR: Automata Part 1"},
 	];
 
 	return entries;

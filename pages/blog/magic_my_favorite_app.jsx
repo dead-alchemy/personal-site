7901f2c50deb3e322c0d54a9deb0d0bfb980eef4
dead-alchemy <dlonicholas@gmail.com>
2023-03-11 21:42:42 -0700

    new blog template and post.

---
+++ pages/blog/magic_my_favorite_app.jsx
@@ -0,0 +1,96 @@
+import {BlogTemplate} from "../../components";
+
+const MagicIsAweseom = () => {
+	return (
+		<BlogTemplate
+			title={{
+				title: "Magic: My Favorite App",
+				subHeading: "A love Letter to Mobile Magic: The Gathering",
+				dateTime: "2023-03-11",
+				humanDate: "March 11th, 2023",
+			}}
+		>
+			<p>
+				In the great world of smart phones and ubiquitous computing,
+				modern marvels have arisen in the form of apps, or small purpose
+				applications. These apps provide hours of utility and fun that
+				make our modern life a little more engaging. We have seen a
+				transition of things once only bound to the physical world move
+				to the digital landscape. One such transition has been games.
+				Once solely in the domain of our physical world, they have made
+				the transition to arcade cabinets, then to computers, to purpose
+				built consoles, and now to the smart phone. One such game that
+				almost seemed impossible to move was the trading card game
+				Magic: The Gathering.
+			</p>
+			<p>
+				Magic: The Gathering is a game with thousands of cards, with an
+				infinite number of combinations of their unique interactions.
+				The game is also considered{" "}
+				<a href="https://arxiv.org/abs/1904.09828">Turing Complete</a>{" "}
+				enabling it to make a computer by various exploitation of its
+				rules and game states. How this game was able to be made into a
+				mobile phone application is nothing short of a miracle. The
+				developer{" "}
+				<a href="https://company.wizards.com/en">
+					Wizards Digital Game Studios
+				</a>{" "}
+				has been able to achieve this impossible task. A game once left
+				only to be played by gathering friends together, going to
+				specialty shops, has now been moved to the phone, freeing the
+				game from its mortal shell, and taking a life in the digital
+				world.{" "}
+				<a href="https://magic.wizards.com/en/mtgarena">Magic Arena</a>,
+				the mobile phone version of Magic: The Gathering, is my favorite
+				app.
+			</p>
+			<p>
+				One of the things that makes me wonder is how this mobile phone
+				game can take such different interactions between various cards
+				and resolve them quickly and accurately. The cards and
+				interactions do not just simply battle each other, but can
+				create new cards, negate others, change the rules of the game,
+				or even the win conditions. This small app can account for all
+				these interactions, make fair and accurate rulings, and do it
+				quickly. To me it is amazing how developers were able to take
+				such a complicated game and make it small enough to be available
+				in a smart phone.
+			</p>
+			<p>
+				The marvel of the smart phone is that it brings ubiquitous
+				computing to the whole world. This computing allows us to do
+				tasks that were once reserved for a proper terminal. With the
+				app Magic: Arena you can play a game of magic quickly and
+				without hassle. In the physical form of the game, set up can be
+				quite cumbersome as individuals will need to shuffle their deck
+				of cards, cut them, draw their starting hands. If they do not
+				like their starting hands, they can mulligan which then starts
+				the shuffling over again. Then space needs to be allocated to
+				play the game. With the app Magic: Arena, this process can take
+				under 10 seconds to play a game instead of several minutes, and
+				without the hassle of freeing up space. Also, games can be
+				played more quickly as time consuming actions like reshuffling
+				decks during a game are automated through the app. It is
+				incredible how quickly a game can be played without having to
+				deal with physical cards. The speed and comfort of playing a
+				game using the app is amazing.
+			</p>
+			<p>
+				What makes this app good is how it’s able to liberate access to
+				this game. Once reserved to just card shops and a high barrier
+				of entry due to its cost, now is free to play for those who are
+				interested. The internet has liberated information and brought
+				the world closer. The app Magic: Arena has been able to bring
+				the game to everyone using the principles that an open and free
+				internet provides. What makes this app good is its ability to
+				bring joy to those who love the game. It may make mistakes due
+				to errors with coding or have some limitations as it cannot make
+				an infinite amount of tokens as can be done in the physical
+				game. However, despite these limitations, anyone can enjoy
+				Magic: The Gathering.
+			</p>
+		</BlogTemplate>
+	);
+};
+
+export default MagicIsAweseom;
